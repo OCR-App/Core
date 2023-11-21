cdfed0f660af2b4a1ee05298e079edfcae50a082
Sajjad <faniam321@gmail.com>
2023-11-21 15:13:40 +0330

    add new field

--- ocr/models.py
+++ ocr/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class ImageLanguages(models.TextChoices):
+    NONE = ("none", "None")
+    PERSIAN = ("persian", "Persian")
+    ENGLISH = ("english", "English")
+
+
 class ImageData(models.Model):
     original_image = models.ImageField(
         _("Original Image"), upload_to="original_image/")
@@ -9,6 +15,8 @@ class ImageData(models.Model):
         _("Edited Image"), upload_to="edited_image/", null=True, blank=True)
     uuid = models.CharField(_("Uuid"), max_length=15)
     result = models.TextField(_("Result"), null=True, blank=True)
+    lang = models.CharField(max_length=7, verbose_name=_(
+        "Language"), choices=ImageLanguages.choices, default=ImageLanguages.NONE)
 
     def __str__(self) -> str:
         return self.uuid
