eaa0e9335ec0974ee2e202a60c822f775fb45f6d
Sajjad <faniam321@gmail.com>
2023-11-21 13:43:43 +0330

    access to media urls

--- ocr_project/urls.py
+++ ocr_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView,
 )
@@ -27,3 +29,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/ocr/', include('ocr.urls'), name='ocr'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
