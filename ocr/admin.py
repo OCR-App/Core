73695edd184e34605886ad87714c4dfd683cc0bc
Sajjad fani <faniam321@gmail.com>
2024-01-09 17:34:22 +0330

    complete admin

--- ocr/admin.py
+++ ocr/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ImageData
 
-# Register your models here.
+
+class ImageDataAdmin(admin.ModelAdmin):
+    list_display = ("uuid",)
+
+
+admin.site.register(ImageData, ImageDataAdmin)
